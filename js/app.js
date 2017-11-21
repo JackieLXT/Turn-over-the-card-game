db53561cc776e75b51236226a9aad7b51d9d6d6d
晓天 <33421489+uuuxiaotian@users.noreply.github.com>
2017-11-22 01:16:12 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,9 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-
 
 $(function(){
 	//页面加载完立即执行
 	xipai();
 
 })
-
-
 var cards = [
     "fa-diamond","fa-diamond",
     "fa-paper-plane-o","fa-paper-plane-o",
@@ -29,15 +14,11 @@ var cards = [
     "fa-bicycle","fa-bicycle",
     "fa-bomb","fa-bomb"
 ];
-
 var cardNum=cards.length;	//记录剩多少张
 var oldid=-1;	//标记
 var n= 0, timer=null;
 var num=0;
 var kong=[];
-
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
@@ -50,9 +31,6 @@ function shuffle(array) {
 
     return array;
 }
-
-
-
 
 function fanpai(id){
 	$("#card"+id).removeAttr("onclick");	//点击第一个时，方块的点击事件移除掉
@@ -172,50 +150,6 @@ function toDub(n){//计时器数字的显示
 	return n<10?"0"+n:""+n;
 }
 
- /*var oTxt=document.getElementsByTagName("input")[0];
- var oStart=document.getElementsByTagName("li");
- oStart.onclick= function () {
-    clearInterval(timer);
-    timer=setInterval(function () {
-      n++;
-      var m=parseInt(n/60);
-      var s=parseInt(n%60);
-      oTxt.value=toDub(m)+":"+toDub(s);
-    },1000/60);
-  };
-  function toDub(n){
-    return n<10?"0"+n:""+n;
-  }*/
 
 
-/*计时器*/
-/*var oStart=document.getElementsByTagName("li");
 
-//var oReset=document.getElementsByTagName("input")[3];
-
-oStart.onclick=function() {
-    clearInterval(timer);
-    timer=setInterval(function () {
-      n++;
-      var m=parseInt(n/60);
-      var s=parseInt(n%60);
-      oTxt.value=toDub(m)+":"+toDub(s);
-    },1000/60);
-  };
-  function () {
-			    clearInterval(timer);
-			  }
-			  function toDub(n){
-			    return n<10?"0"+n:""+n;
-			  }*/
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
